c7de495069af5e4ad89cc8f6909ab003ccb7689b
wongprom <>
2022-04-15 17:47:05 +0200

    Implement dynamic data to card.

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -142,11 +142,6 @@ const NFTDropPage = ({ collection }: Props) => {
         toast.dismiss(notification)
       })
   }
-
-  const numberofNFTs = [
-    3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
-  ]
-
   return (
     <div className="flex h-screen flex-col lg:grid lg:grid-cols-10">
       <Toaster position="top-right" />
@@ -224,21 +219,26 @@ const NFTDropPage = ({ collection }: Props) => {
         <main className="max-w-screen-4xl">
           <section className="w-full rounded-2xl py-20 lg:mt-10 lg:bg-gray-800">
             <div className=" max-w-screen-4xl mx-auto grid grid-cols-1 justify-items-center gap-6 rounded-2xl xl:grid-cols-2 2xl:grid-cols-4 ">
-              {numberofNFTs.map((nft, index) => (
-                <div className="mx-auto grid w-full grid-cols-1 justify-items-center rounded-2xl border bg-gray-900 md:max-w-sm">
+              {/* @ts-ignore */}
+              {unClaimedNFTs?.map((ape) => (
+                // https://tailwindcss.com/docs/transition-duration
+                <div
+                  key={ape.name}
+                  className="group mx-auto grid w-full grid-cols-1 justify-items-center rounded-2xl border bg-gray-900  md:max-w-sm"
+                >
                   {/* <!--1--> */}
                   <div className="flex flex-col">
                     <div className="mx-auto my-5 h-2 w-10 rounded-full border-0 bg-gray-500"></div>
                     <div className="h-52 w-48 rounded-lg">
                       <img
                         className="h-full w-full rounded-lg object-cover"
-                        src="https://gateway.ipfscdn.io/ipfs/QmfMycqNrFwaLKjtnLKt5x63E5BqijPKZXUzXED7x3peLg/4.png"
+                        src={ape.image}
                         alt=""
                       />
                     </div>
                     <div className="-mt-4 flex">
                       <span className="mx-auto rounded-full bg-white px-4 py-1 text-sm">
-                        ape-1837
+                        {ape.name}
                       </span>
                     </div>
                     <div className="mt-5 flex justify-center">
@@ -259,52 +259,38 @@ const NFTDropPage = ({ collection }: Props) => {
                     </div>
                     <div className="flex flex-col items-center justify-between sm:flex-row">
                       <p>Name</p>
-                      <p className="text-gray-50">ape-1837</p>
+                      <p className="text-gray-50">{ape.name}</p>
                     </div>
                     <div className="flex flex-col items-center justify-between sm:flex-row">
                       <p>Collection</p>
-                      <p className="text-gray-50">A WONG Ape</p>
+                      <p className="text-gray-50">{ape.description}</p>
                     </div>
                     <div className="flex flex-col items-center justify-between sm:flex-row">
                       <p>Mint price</p>
-                      <p className="text-gray-50">0.01 ETH</p>
+                      <p className="text-gray-50">{priceInEth} ETH</p>
                     </div>
                   </div>
                   {/* <!--3--> */}
                   <div className="mt-5 w-2/3 rounded-xl bg-gray-800 p-3 text-white">
-                    <h3>Properties</h3>
+                    <h3 className="text-blue-200">Properties</h3>
                     <div className="grid grid-cols-1 gap-2 sm:grid-cols-3">
-                      <div className="mt-2 flex flex-col items-center justify-center rounded-md border  border-blue-400 p-2 ">
-                        <p className="text-xs text-blue-400">Eyes</p>
-                        <p className="text-sm">zombie</p>
-                        <p></p>
-                      </div>
-                      <div className="mt-2 flex flex-col items-center justify-center rounded-md border  border-blue-400 p-2 ">
-                        <p className="text-xs text-blue-400">Fur</p>
-                        <p className="text-sm">Gold</p>
-                        <p></p>
-                      </div>
-                      <div className="mt-2 flex flex-col items-center justify-center rounded-md border  border-blue-400 p-2 ">
-                        <p className="text-xs text-blue-400">Glases</p>
-                        <p className="text-sm">none</p>
-                        <p></p>
-                      </div>
-                      <div className="mt-2 flex flex-col items-center justify-center rounded-md border  border-blue-400 p-2 ">
-                        <p className="text-xs text-blue-400">Hat</p>
-                        <p className="text-sm">none</p>
-                        <p></p>
-                      </div>
-                      <div className="mt-2 flex flex-col items-center justify-center rounded-md border  border-blue-400 p-2 ">
-                        <p className="text-xs text-blue-400">Shirt</p>
-                        <p className="text-sm">polo</p>
-                        <p></p>
-                      </div>
+                      {Object.entries(ape.properties).map((arr) => {
+                        return (
+                          <div className="mt-2 flex flex-col overflow-hidden rounded-md border border-blue-400 p-2 ">
+                            <p className="text-center text-xs text-blue-400">
+                              {arr[0]}
+                            </p>
+                            {/* @ts-ignore */}
+                            <p className="text-center text-sm">{arr[1]}</p>
+                          </div>
+                        )
+                      })}
                     </div>
                   </div>
                   {/* <!--4--> */}
                   <div className="flex w-full flex-col items-center">
-                    <button className="my-10 w-10/12 rounded-full bg-black py-4 text-xl font-medium tracking-widest text-gray-100 shadow-[9px_10px_30px_rgba(240,_46,_170,_0.5)]">
-                      MINT
+                    <button className="my-10 w-10/12 rounded-full bg-black py-4 text-xl font-medium tracking-widest text-gray-100 shadow-[9px_10px_30px_rgba(240,_46,_170,_0.5)] hover:shadow-[9px_10px_30px_rgba(126,_21,_69,_0.5)]">
+                      MINT {priceInEth} ETH
                     </button>
                   </div>
                 </div>
